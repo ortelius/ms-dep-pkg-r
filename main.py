83ac47b1e29cddf4b26a0dfa02de05bc8dc3a35d
Steve Taylor <steve@deployhub.com>
2021-08-09 21:10:04 -0600

    change sql to match new table layout

--- main.py
+++ main.py
@@ -1,52 +1,82 @@
 import os
 
 import psycopg2
-from flask import Flask
+import pybreaker
+import requests
+from flask import Flask, request  # module to create an api
 from flask_restful import Api, Resource
 
-from db_startup import create_dependencies_table, seed_dependencies_table
-
+# Init Flask
 app = Flask(__name__)
 api = Api(app)
+app.url_map.strict_slashes = False
 
-DB_HOST = os.environ.get('DB_HOST', "localhost")
-DB_USER = os.environ.get('DB_USER', "postgres")
-DB_PASSWORD = os.environ.get('DB_PASSWORD', "postgres")
-DB_PORT = os.environ.get('DB_PORT', 5432)
-DB_NAME = os.environ.get('DB_NAME', "postgres")
+# Init db connection
+db_host = os.getenv("DB_HOST", "localhost")
+db_name = os.getenv("DB_NAME", "postgres")
+db_user = os.getenv("DB_USER", "postgres")
+db_pass = os.getenv("DB_PASS", "postgres")
+db_port = os.getenv("DB_PORT", "5432") 
 
-conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD, port=DB_PORT)
+conn_circuit_breaker = pybreaker.CircuitBreaker(
+    fail_max=1,
+    reset_timeout=10,
+)
 
-@app.route('/')
-def homepage():
-    return """<a href="http://localhost:5000/msapi/deppkg"> Click Here"""
+@conn_circuit_breaker
+def create_conn():
+    conn = psycopg2.connect(host=db_host, database=db_name, user=db_user, password=db_pass, port=db_port)
+    return conn
 
 class EnvironmentResource(Resource):
-    def get(self):
-        query = f"SELECT compid, packagename, packageversion, cve, cve_url, license, license_url FROM dm_componentdeps;"
+
+    @classmethod
+    def get(cls):
+        conn = create_conn() 
         cursor = conn.cursor()
-        cursor.execute(query)
+
+        compid = request.args.get('compid', None)
+        deptype = request.args.get('deptype', None)
+
+        sql = "SELECT packagename, packageversion, name, url FROM dm_componentdeps where compid = %s and deptype = %s"
+
+        params=(compid, deptype, )
+        cursor.execute(sql, params)
         rows = cursor.fetchall()
         response_data = []
+        valid_url = {}
+
         for row in rows:
+            # check for license on SPDX site if not found just return the license landing page
+            packagename = row[0]
+            packageversion = row[1]
+            name = row[2]
+            url = row[3]
+
+            if (not url):
+                url = 'https://spdx.org/licenses/'
+
+            if (name not in valid_url):
+                r = requests.head(url)
+                if (r.status_code == 200):
+                    valid_url[name] = url
+                else:
+                    valid_url[name] = 'https://spdx.org/licenses/'
+
+            url = valid_url[name]
+
             response_data.append(
                 {
-                    'compid': row[0],
-                    'packagename': row[1],
-                    'packageversion': row[2],
-                    'cve': row[3],
-                    'cve_url': row[4],
-                    'license': row[5],
-                    'license_url': row[6],
+                    'packagename': packagename,
+                    'packageversion': packageversion,
+                    'name': name,
+                    'url': url
                 }
             )
         cursor.close()
-        return response_data
-
+        return {'data' : response_data}
 
 api.add_resource(EnvironmentResource, '/msapi/deppkg')
 
 if __name__ == '__main__':
-    create_dependencies_table(conn)
-    seed_dependencies_table(conn)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5004)
