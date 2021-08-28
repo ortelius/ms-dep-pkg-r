9c5e3185793c97d016a02f10c02b3b8c4dae0ad0
Utkarsh Kumar Sharma <utkarshkviim@gmail.com>
2021-08-28 11:20:40 +0530

    add swagger, conn pool, exception, cloudbuild, and code refactoring

--- main.py
+++ main.py
@@ -1,47 +1,106 @@
 import os
+from http import HTTPStatus
 
 import psycopg2
 import pybreaker
 import requests
 from flask import Flask, request  # module to create an api
 from flask_restful import Api, Resource
+import sqlalchemy.pool as pool
+from flask_swagger_ui import get_swaggerui_blueprint
+from webargs.flaskparser import abort, parser
+
+#pylint: disable=unused-argument
+
+
+@parser.error_handler
+def handle_request_parsing_error(err, req, schema, *, error_status_code, error_headers):
+    abort(HTTPStatus.BAD_REQUEST, errors=err.messages)
+
 
 # Init Flask
 app = Flask(__name__)
 api = Api(app)
 app.url_map.strict_slashes = False
 
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
+
+# swagger config
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.yml'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "ortelius-ms-dep-pkg-r"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
 # Init db connection
 db_host = os.getenv("DB_HOST", "localhost")
 db_name = os.getenv("DB_NAME", "postgres")
 db_user = os.getenv("DB_USER", "postgres")
 db_pass = os.getenv("DB_PASS", "postgres")
-db_port = os.getenv("DB_PORT", "5432") 
+db_port = os.getenv("DB_PORT", "5432")
 validateuser_url = os.getenv("VALIDATEUSER_URL", "http://localhost:5000")
+
+# connection pool config
+conn_pool_size = int(os.getenv("POOL_SIZE", "3"))
+conn_pool_max_overflow = int(os.getenv("POOL_MAX_OVERFLOW", "2"))
+conn_pool_timeout = float(os.getenv("POOL_TIMEOUT", "30.0"))
 
 conn_circuit_breaker = pybreaker.CircuitBreaker(
     fail_max=1,
     reset_timeout=10,
 )
 
+
 @conn_circuit_breaker
 def create_conn():
     conn = psycopg2.connect(host=db_host, database=db_name, user=db_user, password=db_pass, port=db_port)
     return conn
 
+
+# connection pool init
+mypool = pool.QueuePool(create_conn, max_overflow=conn_pool_max_overflow, pool_size=conn_pool_size, timeout=conn_pool_timeout)
+
+
+class HealthCheck(Resource):
+    def get(self):
+        try:
+            conn = mypool.connect()
+            cursor = conn.cursor()
+            cursor.execute('SELECT 1')
+            conn.close()
+            if cursor.rowcount > 0:
+                return ({"status": 'UP', "service_name": 'ortelius-ms-dep-pkg-r'}), HTTPStatus.OK
+            return ({"status": 'DOWN'}), HTTPStatus.SERVICE_UNAVAILABLE
+
+        except Exception as err:
+            print(err)
+            return ({"status": 'DOWN'}), HTTPStatus.SERVICE_UNAVAILABLE
+
+
+api.add_resource(HealthCheck, '/health')
+
+
 class EnvironmentResource(Resource):
 
-    @classmethod
-    def get(cls):
-        
+    def get(self):
+
         result = requests.get(validateuser_url + "/msapi/validateuser", cookies=request.cookies)
         if (result is None):
-            return None, 404
+            return None, HTTPStatus.UNAUTHORIZED
 
         if (result.status_code != 200):
-            return result.json(), 404
-        
-        conn = create_conn() 
+            return result.json(), HTTPStatus.UNAUTHORIZED
+
+        conn = mypool.connect()
         cursor = conn.cursor()
 
         compid = request.args.get('compid', None)
@@ -49,7 +108,7 @@ class EnvironmentResource(Resource):
 
         sql = "SELECT packagename, packageversion, name, url, summary FROM dm_componentdeps where compid = %s and deptype = %s"
 
-        params=(compid, deptype, )
+        params = (compid, deptype, )
         cursor.execute(sql, params)
         rows = cursor.fetchall()
         response_data = []
@@ -85,7 +144,8 @@ class EnvironmentResource(Resource):
                 }
             )
         cursor.close()
-        return {'data' : response_data}
+        return {'data': response_data}
+
 
 api.add_resource(EnvironmentResource, '/msapi/deppkg')
 
